a028bf0b3dd6745ab0fcf1e19ea6e029cf9cfaa1
Leila Lin <leila.cy.lin@gmail.com>
2017-03-18 02:00:49 +0800

    Create project.

---
+++ app/src/main/java/lin/leila/petshopinspector/ShopDetailActivity.java
@@ -0,0 +1,51 @@
+package lin.leila.petshopinspector;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by Leila on 2017/3/16.
+ */
+
+
+public class ShopDetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_NAME = "cheese_name";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        final String cheeseName = intent.getStringExtra(EXTRA_NAME);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(cheeseName);
+
+        loadBackdrop();
+    }
+
+    private void loadBackdrop() {
+        final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
+        Glide.with(this).load(Shops.getRandomShopDrawable()).centerCrop().into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sample_actions, menu);
+        return true;
+    }
+}
