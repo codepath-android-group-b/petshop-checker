ba1654127e04f687f9d1e1a5e5b699296f7d8bfe
javiosyc <javiosyc@gmail.com>
2017-03-18 20:17:19 +0800

    add Petshop model

---
+++ app/src/main/java/lin/leila/petshopinspector/models/PetShop.java
@@ -0,0 +1,89 @@
+package lin.leila.petshopinspector.models;
+
+/**
+ * Created by javiosyc on 2017/3/18.
+ */
+
+public class PetShop {
+    private String city;
+    private String district;
+    private String type;
+    private String assistant;
+    private String shopName;
+    private String manager;
+    private String address;
+    private String services;
+    private String certGrade;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAssistant() {
+        return assistant;
+    }
+
+    public void setAssistant(String assistant) {
+        this.assistant = assistant;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getServices() {
+        return services;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
+    }
+
+    public String getCertGrade() {
+        return certGrade;
+    }
+
+    public void setCertGrade(String certGrade) {
+        this.certGrade = certGrade;
+    }
+}
---
+++ app/src/main/java/lin/leila/petshopinspector/models/PetShopQueryCondition.java
@@ -0,0 +1,35 @@
+package lin.leila.petshopinspector.models;
+
+/**
+ * Created by javiosyc on 2017/3/18.
+ */
+
+public class PetShopQueryCondition {
+    private String city;
+    private String district;
+    private String type;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
